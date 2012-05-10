6f7a36bf85ce2dfc5ccb24e41fad45091baad9eb
Daniel Gryniewicz <dang@ghs.com>
2012-05-10 09:26:31 -0400

    Initial commit of GTWS

---
+++ env.sh
@@ -0,0 +1,95 @@
+
+function tools {
+	if [ -z "${1}" ]; then
+		echo "tools [<version>] <workspace>"
+		return 0
+	fi
+	if [ -z "${2}" ]; then
+		WSVER="trunk"
+		WSNAME="${1}"
+	else
+		WSVER="${1}"
+		WSNAME="${2}"
+	fi
+	if [ ! -f ${HOME}/src/${WSVER}/${WSNAME}/.tools.env ]; then
+		echo "No env for ${WSVER}/${WSNAME}"
+		return 1
+	else
+		. ${HOME}/src/${WSVER}/${WSNAME}/.tools.env
+	fi
+}
+
+function gatt() {
+	TGT="${1}"
+	shift
+	case "${TGT}" in
+		ospf3)
+			cd ${GATED_TOOLS_PATH}/tests/cli/system/ospf3/"$@"
+			;;
+		ospf2)
+			cd ${GATED_TOOLS_PATH}/tests/cli/system/ospf/"$@"
+			;;
+		bgp)
+			cd ${GATED_TOOLS_PATH}/tests/cli/system/bgp/"$@"
+			;;
+		mpbgp)
+			cd ${GATED_TOOLS_PATH}/tests/cli/system/mpbgp/"$@"
+			;;
+		mpbgpv4v6)
+			cd ${GATED_TOOLS_PATH}/tests/cli/system/mpbgpv4v6/"$@"
+			;;
+		rip2)
+			cd ${GATED_TOOLS_PATH}/tests/cli/system/rip/"$@"
+			;;
+		ripng)
+			cd ${GATED_TOOLS_PATH}/tests/cli/system/ripng/"$@"
+			;;
+		leak)
+			cd ${GATED_TOOLS_PATH}/tests/cli/system/leak/"$@"
+			;;
+		scripts)
+			cd ${GATED_TOOLS_PATH}/scripts/"$@"
+			;;
+		data)
+			cd ${GATED_TOOLS_PATH}/data/"$@"
+			;;
+		*)
+			cd ${GATED_TOOLS_PATH}/"${TGT}"/"$@"
+			;;
+	esac
+}
+
+function cdws() {
+	cd "${HOME}/src/${WSVER}/${CURWS}/$@"
+}
+
+function debugws() {
+	if [ -z "${3}" ]; then
+		echo "debugws <gversion> <wsname> <iversion>"
+		return 1
+	else
+		TGVER="${1}"
+		TWS="${2}"
+		TIVER="${3}"
+	fi
+	case "${TIVER}" in
+		"itrunk")
+			TBIN="bin/pcx86"
+			;;
+		"i11.0")
+			TBIN="bin/pcx86"
+			;;
+		"i10.0")
+			TBIN="pcx86"
+			;;
+		"i5.0")
+			TBIN="pcx86"
+			;;
+		"i5.0-vrf")
+			TBIN="pcx86"
+			;;
+	esac
+
+	cd "${HOME}/src/${TGVER}/${TWS}/${TIVER}/${TBIN}"
+}
+
---
+++ gtws.sh
@@ -0,0 +1,28 @@
+#
+# Functions for gtws
+#
+
+die() {
+	echo "$@"
+	exit 1
+}
+
+usage() {
+	local myusage;
+	if [ -n "${USAGE}" ]; then
+		myusage=${USAGE}
+	else
+		myusage="No usage given"
+	fi
+	if [ -n "$1" ]; then
+		echo "$@"
+	fi
+	echo ""
+	echo "Usage:"
+	echo "`basename $0` ${myusage}"
+	if [ -n "${LONGUSAGE}" ]; then
+		echo -e "${LONGUSAGE}"
+	fi
+	exit 1
+}
+
